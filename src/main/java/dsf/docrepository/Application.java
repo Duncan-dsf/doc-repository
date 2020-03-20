2e99116de189e25bb27a7ce6f1e473c82ca7c427
dongshaofei3 <dongshaofei3@jd.com>
2020-03-20 22:22:55 +0800

    初始化

---
+++ src/main/java/dsf/docrepository/Application.java
@@ -0,0 +1,17 @@
+/*
+ * @(#)Application.java 1.0 2020/3/17
+ *
+ * Copyright (c) 2019 JDD. All rights reserved.
+ * PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package dsf.docrepository;
+
+/**
+ * TODO
+ *
+ * @author dongshaofei3
+ * @version 1.0
+ * @since 1.0
+ */
+public class Application {
+}
